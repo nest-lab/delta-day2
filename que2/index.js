c3f11593958ed4f72c1f9d2af2068bda3735d647
Karim Ifedayo <karimjnr@gmail.com>
2017-08-22 13:37:59 -0700

    Que 2 solved

---
+++ que2/index.js
@@ -0,0 +1,49 @@
+var request  = require("request");
+
+console.log("Please write the company Stock name");
+console.log("Like 'GOOGL' for google, 'FB' for facebook");
+process.stdin.on("data", function (userInputedUrl) {
+    if (userInputedUrl.toString() == "exit") {
+        process.exit()
+    }
+    console.log("loading...");
+    // Making Request
+    var stockName = userInputedUrl.toString();
+    var url = `http://finance.yahoo.com/d/quotes.csv?s=${stockName}&f=m7` ;
+    request(url, function (err, response, html) {
+        
+        
+        if (!err) {
+            if (Number(html) < 0) {
+                console.log("\n\n\n\n")
+                console.log(`The Company with Stock name ${stockName}, performed poorly `)
+                console.log("\n\n\n\n")
+            }
+            else if (Number(html) == 0) {
+                console.log("\n\n\n\n")
+                console.log(`The Company with Stock name ${stockName}, did not have any losss or profit `)
+                console.log("\n\n\n\n")
+            }
+
+            else  if (Number(html) > 1){
+                console.log("\n\n\n\n")
+                console.log(`The Company with Stock name ${stockName}, did well `);
+                console.log("\n\n")
+            }
+            else {
+                console.log(`There is no  Company with Stock name ${stockName}`);
+            }
+            console.log("You can check another Business organization");
+             console.log("Or Type 'exit' to exit the command line or Press CTRL + C");
+        }
+        else{
+            console.log("Invalid URL or no internet network \n Try again")
+        }
+    })
+
+})
+
+
+process.on("exit", function () {
+    console.log("Have a nice day");
+})
---
+++ que2/readme.md
@@ -0,0 +1,6 @@
+# Solved Question 2 day 2
+Run "npm install" to install dependencies
+
+Then run node index
+
+Then follow the command Line
